ace84a649ceea92a4f94963fd1aa649e2406597b
Nodirbek <nodirbekiskandarov2003@gmail.com>
2023-12-30 11:47:20 +0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    // Handle button press
+    console.log('Button pressed!');
+    Alert.alert(
+      'begzodgulmi',
+      'ha yo yoq',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+          onPress: () => Alert.alert(
+            'javob xato'
+          )
+        },
+        { text: 'OK', onPress: () => Alert.alert(
+          'togri javob uchun raxmat',
+        ) },
+      ],
+      { cancelable: false }
+    )
+  };
+
+  const handlePress2 = () => {
+    Alert.prompt("nimadir kiriting", "nima", text => console.log(text + "gul"))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>bekzod</Text>
+      <Button
+        title="Press me"
+        onPress={handlePress}
+        style={styles.button}
+      />
+      <Button
+        title="Press prompt button"
+        onPress={handlePress2}
+        style={styles.button}
+      />
+
+      <Image source={require('./assets/adaptive-icon.png')} style={styles.image}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +54,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "red",
+  },
+  button: {
+    width: "100px",
+    color: "red",
+    display: "none"
+  },
+  image: {
+    width: 200,
+    height: 200
+  }
 });
